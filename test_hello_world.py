dc6a68e76aee7d9b3f57a72c618ab0763692c964
Bryan Schwab <bschwab@gmail.com>
2019-05-24 14:39:45 -0700

    fixing tests lol

--- test_hello_world.py
+++ test_hello_world.py
@@ -13,7 +13,7 @@ class TestHelloWorld(unittest.TestCase):
     
     def test_message(self):
         response = self.app.get('/')
-        message = hello_world.wrap_html('I like whales!')
+        message = hello_world.wrap_html('I like whales, too!')
         self.assertEqual(response.data, message)
 
 if __name__ == '__main__':
